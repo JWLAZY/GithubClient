7a4010041a767e3f7cb03e451792fde4c752989e
郑建文 <zhengjianwen@outlook.com>
2016-09-01 13:37:06 +0800

    [update] ad

--- YZGithubTests/YZGithubTests.swift
+++ YZGithubTests/YZGithubTests.swift
@@ -2,8 +2,8 @@
 //  Created by www.52learn.wang.
 //  Copyright © 2016年 www.52learn.wang. All rights reserved.
 //  QQ群－》ObjC:343640780 Swift:172090834 ReactNative:555705178
-//  博客：http://www.52learn.wang//  Github: https://github.com/wu2LearnTeam
-//  微博：http://weibo.com/mobiledevelopment
+//  博客：http://www.52learn.wang
+//  Github: https://github.com/YZMobileTalks
 //
 import XCTest
 @testable import YZGithub
--- head.py
+++ head.py
@@ -19,15 +19,16 @@ def getfileName(path):
                 f=open(os.path.join(path,l),'r+')
                 flist=f.readlines(8)
 #                 print flist
-                flist[0] = "//\n"
-                flist[1] = "//  Created by www.52learn.wang.\n"
-                flist[2] = "//  Copyright © 2016年 www.52learn.wang. All rights reserved.\n"
-                flist[3] = "//  QQ群－》ObjC:343640780 Swift:172090834 ReactNative:555705178\n"
-                flist[4] = "//  博客：http://www.52learn.wang"
-                flist[5] = "//  Github: https://github.com/wu2LearnTeam\n"
-                flist[6] = "//  微博：http://weibo.com/mobiledevelopment\n"
-                flist[7] = "//\n"
-                flist.insert(8,"")
+                # flist[0] = "//\n"
+                # flist[1] = "//  Created by www.52learn.wang.\n"
+                # flist[2] = "//  Copyright © 2016年 www.52learn.wang. All rights reserved.\n"
+                # flist[3] = "//  QQ群－》ObjC:343640780 Swift:172090834 ReactNative:555705178\n"
+                flist[4] = "//  博客：http://www.52learn.wang\n"
+                flist[5] = "//  Github: https://github.com/YZMobileTalks\n"
+                # flist[6] = "//  微博：http://weibo.com/mobiledevelopment\n"
+                # flist[7] = "//\n"
+                # flist.insert(8,"")
+                flist.insert(9,"")
                 f=open(os.path.join(path,l),'w+')
                 f.writelines(flist)
 #                 print('----' + f.readlines(1)[0])
