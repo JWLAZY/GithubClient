8060ae57acc1b84d3bd268e84b4c6d2346e8d925
郑建文 <zhengjianwen@outlook.com>
2016-12-01 00:53:42 +0800

    [updata] 替换掉 sataic 的cell

---
+++ YZGithub/Sences/Profile/View/SettingCell.swift
@@ -0,0 +1,24 @@
+//
+//  SettingCell.swift
+//  YZGithub
+//
+//  Created by 郑建文 on 16/11/30.
+//  Copyright © 2016年 Zheng. All rights reserved.
+//
+
+import UIKit
+
+class SettingCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
